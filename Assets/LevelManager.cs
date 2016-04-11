882db2afe5de484da6e30f5d9656feed4939335f
Miliko <aberrau@gmail.com>
2016-04-11 17:26:05 +0200

    first commit

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+	public void LoadLevel(string name)
+	{
+		Debug.Log ("Level selected: " + name);
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest()
+	{
+		Debug.Log ("Quited requested");
+		Application.Quit();
+	}
+}
---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class NumberWizard : MonoBehaviour
+{
+	int max;
+	int min;
+	int guess;
+	public int counter;
+	public Text textbox;
+	
+	void Start ()
+	{
+	Startgame();
+	}
+	
+	
+	void Startgame ()
+	{
+		this.max = 1000;
+		this.min = 1;
+		this.guess = 500;
+		this.max = max + 1;
+		this.textbox.text = guess.ToString();
+	}
+	
+	
+	void Nextguess()
+	{
+		guess = (min + max)/2;
+		counter +=1;
+		if (counter <= 5)
+		{
+			textbox.text = guess.ToString();
+		}
+		else
+		{
+			Application.LoadLevel("Win");
+		}
+	}
+	
+	
+	public void higher()
+	{
+		min = guess;
+		Nextguess ();
+	}
+
+
+	public void lower()
+	{
+		max = guess;
+		Nextguess ();
+	}
+	
+	
+}
